dcd66c1b0556e399cf2c4a07204aa8da1ca4d706
Jean-Yves <jeanyves.lemelle@gmail.om>
2014-06-05 18:55:16 +0200

    add MC-Prospect folder

---
+++ MC-Prospect/_openerp_.py
@@ -0,0 +1,10 @@
+{
+    'name': "MC university prospects",
+    'version': "1.0",
+    'author': "jeanyvesl",
+    'category': "Tools",
+    'depends': ['res_partner'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+}
